9ec7fae8dc8f7f7f61d6901e9bc313365a59aa6d
Deivid Silva <imdeividsilva@gmail.com>
2023-06-14 14:06:22 -0300

    transformação do arquivo json para ts

---
+++ Database.ts
@@ -0,0 +1,48 @@
+const alimentos: object[] = [
+    {
+        nome: 'arroz tipo 1 cozido',
+        cod: 'ar-1-co',
+        carb: 28.1,
+        prot: 2.5,
+        gor: 0.2,
+        cal: 128
+    },
+    
+    {
+        nome: 'feijão preto cozido',
+        cod: 'fj-pr-co',
+        carb: 14,
+        prot: 4.5,
+        gor: 0.5,
+        cal: 77
+    },
+
+    {
+        nome: 'banana prata crua',
+        cod: 'bn-pt-cr',
+        carb: 26,
+        prot: 1.3,
+        gor: 0.1,
+        cal: 98
+    },
+
+    {
+        nome: 'frango peito cru',
+        cod: 'fg-pt-cr',
+        carb: 0,
+        prot: 21.5,
+        gor: 3,
+        cal: 119
+    },
+
+    {
+        nome: 'batata inglesa cozida',
+        cod: 'bt-in-co',
+        carb: 11.9,
+        prot: 1.2,
+        gor: 0,
+        cal: 52
+    }
+]
+
+export default alimentos;
